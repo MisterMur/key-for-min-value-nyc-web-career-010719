1571e9c47f475241d6c01b8bf63d08b4c0dbd591
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-03 17:06:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   end
-  smallest =name_hash[0]
+  smallest =name_hash
   name_hash.collect do |key,value|
     if value<smallest
       smallest = value
